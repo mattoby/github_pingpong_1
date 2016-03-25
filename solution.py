cbbe4c3fedd377b6d563dc8dac4d6e1cbd24cad8
Emmanuel Awa <eawa@brandeis.edu>
2016-03-25 15:47:34 -0400

    WIP: starting the process of holding a pointer to the first element and then seeing if the element and the next element adds to the next->next element

--- solution.py
+++ solution.py
@@ -1,20 +1,26 @@
 class Solution(object):
-    def isAdditiveNumber(self, num):
+    def isAdditiveNumber(self, nums):
         """
         :type num: str
         :rtype: bool
         """
         if not nums or len(nums) <= 2:
             return False
-        
+        # nums = int(nums)
         n = len(nums)
         i = 0
         j = 0
+        prev = int(nums[0])
         
         '''
             Hold a pointer to the last time we found a valid additive sum
         '''
-        for k in xrange(n):
-            
+        for k in xrange(1, n):
+            if k < n-1 and  int(nums[k]) + prev == int(nums[k+1]):
+                prev = int(nums[k])
+                print prev, nums[k], nums[k+1]
+            else:
+                return False
+        return True
         
         
